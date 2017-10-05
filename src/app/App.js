3c99e267e21d251b8939d6472d968b098939be3c
mattia richetto <mattia.richetto@gmail.com>
2017-10-04 23:04:31 -0700

    fetch from client (WIP)

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const App = ({ name, version, imgSrc }) => { // eslint-disable-line
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12">
-          <h1>{name}</h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleOnClick = () => {
+    console.log('Moar ...');
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1>{this.props.name}</h1>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-12 text-center">
-          <img src={imgSrc} className="img-fluid border border-dark" alt="Powered By GIPHY" />
+        <div className="row">
+          <div className="col-12 text-center">
+            <img
+              src={this.props.imgSrc}
+              className="img-fluid border border-dark"
+              alt="Powered By GIPHY"
+            />
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <div className="text-center">
-            {(new Date()).getFullYear()} {name} v{version}
+        <div className="row">
+          <div className="col-12 text-center p-2">
+            <button type="button" className="btn btn-primary" onClick={this.handleOnClick}>
+              Moar ...
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="text-center">
+              {(new Date()).getFullYear()} {this.props.name} v{this.props.version}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  version: PropTypes.string.isRequired
 };
 
-module.exports = App;
+export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ const React = require('react');
 const { renderToString } = require('react-dom/server');
 const fetch = require('node-fetch');
 const debug = require('debug')('blue-sky-action:routes/index');
-const App = require('../../dist/bundle');
+const App = require('../../dist/bundle').default;
 const { name, version } = require('../../package.json');
 
 const router = express.Router();
 
-async function fetchImgSrc() {
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=&rating=G`;
+async function fetchImgSrc({ apiKey, tag }) {
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${tag}&rating=G`;
   return fetch(url)
     .then(response => response.json())
     .then(({ data }) => data.image_url)
@@ -20,7 +20,10 @@ async function fetchImgSrc() {
 }
 
 router.get('/', async (req, res) => {
-  const imgSrc = await fetchImgSrc();
+  const imgSrc = await fetchImgSrc({
+    apiKey: process.env.GIPHY_API_KEY,
+    tag: process.env.GIPHY_TAG
+  });
   const props = { name, version, imgSrc };
   const app = renderToString(React.createElement(App, props));
   const model = { title: name, app, props };
