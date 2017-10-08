91717d9b785b97999b70007d9fcfc297346f4cab
mattia richetto <mattia.richetto@gmail.com>
2017-10-07 17:39:09 -0700

    use react dom hydrate

--- src/app/index.client.js
+++ src/app/index.client.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import App from './App';
 
 const root = document.getElementById('app');
 
 const props = window.__MODEL__;
 
-ReactDOM.render(<App {...props} />, root);
+hydrate(<App {...props} />, root);
