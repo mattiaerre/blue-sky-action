8021c5872929280fa594ac9ca449c1fcb72e64ce
mattia richetto <mattia.richetto@gmail.com>
2017-10-15 14:39:47 -0700

    sortBy no more

--- src/routes/fetch-articles.js
+++ src/routes/fetch-articles.js
@@ -2,20 +2,25 @@ const appendQuery = require('append-query');
 const debug = require('debug')('blue-sky-action:routes/fetch-articles');
 const fetch = require('node-fetch');
 
-async function fetchArticles({ apiKey, baseUrl, sortBy, source }) {
+async function fetchArticles({ apiKey, baseUrl, source }) {
   const params = {
     apiKey,
-    sortBy,
     source
   };
   const url = appendQuery(`${baseUrl}/v1/articles`, params);
 
   return fetch(url)
     .then(response => response.json())
-    .then(({ articles }) => articles)
+    .then(({ status, code, message, articles }) => {
+      if (status === 'error') {
+        throw new Error(`${status}, ${code}, ${message}`);
+      } else {
+        return articles;
+      }
+    })
     .catch(error => {
       debug(error);
-      return 'error';
+      return []; // info: not too sure about this
     });
 }
 
