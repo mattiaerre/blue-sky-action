f5a4323c67ec9bb535835f8a90332810cf4030a1
mattia richetto <mattia.richetto@gmail.com>
2017-11-04 20:19:55 -0700

    add vegas

---
+++ src/data/fetch-current-weather-by-city-name.test.js
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import fetch from 'node-fetch';
+
+config();
+
+const CITY_NAME = 'Las Vegas';
+
+describe('fetch current weather by city name', () => {
+  test(CITY_NAME, async () => {
+    const url = `${process.env
+      .OPEN_WEATHER_MAP_BASE_URL}/data/2.5/weather?q=${CITY_NAME}&appid=${process
+      .env.OPEN_WEATHER_MAP_API_KEY}`;
+    const weather = await fetch(url)
+      .then(response => response.json())
+      .then(data => data);
+
+    expect(weather).toMatchSnapshot();
+  });
+});
