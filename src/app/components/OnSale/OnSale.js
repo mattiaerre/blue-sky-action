fc6c33f709ee5c6d58cd69c4d09c5c4b503071ff
mattia richetto <mattia.richetto@gmail.com>
2017-11-08 23:49:35 -0800

    on sale

--- src/app/components/OnSale/OnSale.js
+++ src/app/components/OnSale/OnSale.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import onSale from './on-sale.json';
 
-const OnSale = () => <div>OnSale</div>;
+const Card = ({ item }) => (
+  <div className="card">
+    <img src={`/images/${item.image}`} className="card-img-top" alt="" />
+    <div className="card-body">
+      <h3 className="card-title">{item.name}</h3>
+      <h4>{item['model-number']}</h4>
+      <p className="card-text">{item.date}</p>
+    </div>
+  </div>
+);
+
+Card.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+const OnSale = () => (
+  <div>
+    <div className="row">
+      <div className="col-12">
+        <h2>On Sale</h2>
+      </div>
+    </div>
+    <div className="row">
+      {Object.keys(onSale).map(key => (
+        <div className="col-md-4 col-sm-6 mb-4" key={key}>
+          <Card item={onSale[key]} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default OnSale;
