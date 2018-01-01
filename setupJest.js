54585ba81bc3d591dfcced46ed65acc053f0e822
mattia richetto <mattia.richetto@gmail.com>
2018-01-01 22:34:38 +0100

    fix test

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,7 @@
-global.fetch = require('jest-fetch-mock');
+const fetch = require('jest-fetch-mock');
+const jestMockNow = require('jest-mock-now');
+
+global.fetch = fetch;
+
+const now = new Date('2018-01-01');
+jestMockNow(now);
