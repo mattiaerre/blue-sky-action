134df13adc69c131f8aa5fe76b0e194739e5e58f
mattia richetto <mattia.richetto@gmail.com>
2018-01-07 16:03:46 +0100

    moar test

--- __tests__/middlewares/graphql.test.js
+++ __tests__/middlewares/graphql.test.js
@@ -1,51 +1,133 @@
 require('dotenv').config();
 const middleware = require('../../src/middlewares/graphql');
 
-const query = `
-{
-  buyPrice(currencyPair: "BTC-USD") {
-    amount
-    base
-    code
-    color
-    currency
-    name
-  }
-  currencies {
-    id
-    name
-  }
-  name
-  spotPrices(currency: "USD") {
-    amount
-    base
-    code
-    color
-    currency
-    name
-  }
-}
-`;
-
-it('full query', async () => {
-  const req = {
-    body: { query },
-    headers: {
-      'Content-Type': 'application/json'
+describe('graphql middleware', () => {
+  const scenarios = [
+    {
+      description: 'query Articles',
+      query: `
+      query Articles {
+        articles(q: "bitcoin") {
+          author
+          description
+          publishedAt
+          source {
+            id
+            name
+          }
+          title
+          url
+          urlToImage
+        }
+      }
+      `
     },
-    method: 'POST'
-  };
+    {
+      description: 'query BuyPrice',
+      query: `
+      query BuyPrice {
+        buyPrice(currencyPair: "BTC-USD") {
+          amount
+          base
+          code
+          color
+          currency
+          name
+        }
+      }
+      `
+    },
+    {
+      description: 'query Currencies',
+      query: `
+      query Currencies {
+        currencies {
+          id
+          name
+        }
+      }
+      `
+    },
+    {
+      description: 'query ExchangeRates',
+      query: `
+      query ExchangeRates {
+        exchangeRates(currency: "USD") {
+          currency
+          rates {
+            currency
+            value
+          }
+        }
+      }
+      `
+    },
+    {
+      description: 'query Name',
+      query: `
+      query Name {
+        name
+      }
+      `
+    },
+    {
+      description: 'query SpotPrices',
+      query: `
+      query SpotPrices {
+        spotPrices(currency: "USD") {
+          amount
+          base
+          code
+          color
+          currency
+          name
+        }
+      }
+      `
+    },
+    {
+      description: 'query TopHeadlines',
+      query: `
+      query TopHeadlines {
+        topHeadlines(source: "cnn") {
+          author
+          description
+          publishedAt
+          source {
+            id
+            name
+          }
+          title
+          url
+          urlToImage
+        }
+      }
+      `
+    }
+  ];
 
-  const res = {
-    setHeader: jest.fn(),
-    end: jest.fn()
-  };
+  scenarios.map(({ description, query }) =>
+    it(description, async () => {
+      const req = {
+        body: { query },
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      };
 
-  const next = jest.fn();
+      const res = {
+        setHeader: jest.fn(),
+        end: jest.fn()
+      };
 
-  await middleware(req, res, next);
+      const next = jest.fn();
 
-  expect(res.setHeader).toBeCalled();
+      await middleware(req, res, next);
 
-  expect(JSON.parse(res.end.mock.calls.toString())).toMatchSnapshot();
+      expect(res.setHeader).toBeCalled();
+
+      expect(JSON.parse(res.end.mock.calls.toString())).toMatchSnapshot();
+    })
+  );
 });
