4a58a7ba7fbbc931d1fefdc6c23163e5bcc1351a
mattia richetto <mattia.richetto@gmail.com>
2018-01-15 20:46:28 -0800

    add log ip

--- src/routes/fetch-datetime.js
+++ src/routes/fetch-datetime.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 
 async function fetchDatetime(ip) {
+  console.log('ip:', ip);
   const url = `${process.env.TIMEZONEAPI_BASE_URL}/api/ip/?ip=${ip}`;
   return fetch(url)
     .then(response => response.json())
