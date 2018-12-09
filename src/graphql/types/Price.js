8126eda4779ca29bd79078f064258aff3b1507ca
mattia richetto <mattia.richetto@gmail.com>
2018-12-08 20:42:17 -0800

    moar prices and cities

--- src/graphql/types/Price.js
+++ src/graphql/types/Price.js
@@ -1,11 +1,16 @@
 const { GraphQLFloat, GraphQLObjectType, GraphQLString } = require('graphql');
 
 const currency = {
+  BAT: {
+    color: 'rgb(237, 93, 42)',
+    foregroundColor: 'white',
+    name: 'Basic Attention Token'
+  },
   BCH: { color: '#8DC451', foregroundColor: 'black', name: 'Bitcoin Cash' },
   BTC: { color: '#FFB119', foregroundColor: 'black', name: 'Bitcoin' },
   CAD: { color: '#FFFFFF', foregroundColor: 'black', name: 'Canadian Dollar' },
   ETC: { color: '#59D4AF', foregroundColor: 'black', name: 'Ethereum Classic' },
-  ETH: { color: '#6F7CBA', foregroundColor: 'black', name: 'Ethereum' },
+  ETH: { color: '#6F7CBA', foregroundColor: 'white', name: 'Ethereum' },
   EUR: { color: '#FFFFFF', foregroundColor: 'black', name: 'Euro' },
   GBP: { color: '#FFFFFF', foregroundColor: 'black', name: 'British Pound' },
   LTC: { color: '#B5B5B5', foregroundColor: 'black', name: 'Litecoin' },
@@ -14,8 +19,28 @@ const currency = {
     foregroundColor: 'white',
     name: 'USD Coin'
   },
+  ZEC: {
+    color: 'rgb(228, 179, 89)',
+    foregroundColor: 'black',
+    name: 'Zcash'
+  },
   ZRX: { color: '#302C2C', foregroundColor: 'white', name: '0x' }
 };
+
+/*
+[ { base: 'BTC', currency: 'USD', amount: '3249.66' },
+  { base: 'BCH', currency: 'USD', amount: '100.24' },
+  { base: 'ETH', currency: 'USD', amount: '82.69' },
+  { base: 'ETC', currency: 'USD', amount: '3.45' },
+  { base: 'LTC', currency: 'USD', amount: '23.18' },
+  { base: 'ZRX', currency: 'USD', amount: '0.29' },
+  { base: 'USDC', currency: 'USD', amount: '1.00' },
+  { base: 'BAT', currency: 'USD', amount: '0.12' },
+  { base: 'ZEC', currency: 'USD', amount: '50.56' },
+  { base: 'EUR', currency: 'USD', amount: '1.14' },
+  { base: 'GBP', currency: 'USD', amount: '1.28' },
+  { base: 'CAD', currency: 'USD', amount: '0.75' } ]
+*/
 
 const Price = new GraphQLObjectType({
   name: 'Price',
