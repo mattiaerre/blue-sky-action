ace322da417bd7e1d17224a6844c50f31c812280
mattia richetto <mattia.richetto@gmail.com>
2019-04-15 17:35:27 -0700

    force-https (#17)
    * force-https
    
    * bump version

---
+++ src/middlewares/requireHTTPS.js
@@ -0,0 +1,13 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get('x-forwarded-proto') !== 'https' &&
+    process.env.NODE_ENV !== 'development'
+  ) {
+    return res.redirect(`https://${req.get('host')}${req.url}`);
+  }
+  return next();
+}
+
+module.exports = requireHTTPS;
