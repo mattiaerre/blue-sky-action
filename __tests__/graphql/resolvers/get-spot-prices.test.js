5f2cbea2097d326878cc1dd658e122a7e77b12e0
mattia richetto <mattia.richetto@gmail.com>
2020-01-16 18:29:24 -0800

    moar-prices (#19)
    * add currencies
    
    * bump version

---
+++ __tests__/graphql/resolvers/get-spot-prices.test.js
@@ -0,0 +1,17 @@
+const getSpotPrices = require('../../../src/graphql/resolvers/get-spot-prices');
+// const currency = require('../../../src/data/currency');
+
+test('to match snapshot', async () => {
+  const args = { currency: 'USD' };
+  const context = {
+    config: { coinbase: { baseUrl: 'https://api.coinbase.com' } }
+  };
+
+  const spotPrices = (await getSpotPrices(null, args, context)).map(
+    ({ amount, ...rest }) => ({ ...rest })
+  );
+
+  expect(spotPrices).toMatchSnapshot();
+
+  // console.log(spotPrices.filter(price => currency[price.base] === undefined));
+});
